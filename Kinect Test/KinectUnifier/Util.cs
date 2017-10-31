2644e5e27dcae9abfac65c75c6398cbf50690bc8
brocny <miso.belak@gmail.com>
2017-10-31 12:27:47 +0100

    Implemented some utility functions

--- Kinect Test/KinectUnifier/Util.cs
+++ Kinect Test/KinectUnifier/Util.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
-using System.Runtime.CompilerServices;
 
 
 namespace KinectUnifier
 {
+    public static class Util
+    {
+        static IDictionary<JointType, Point2F> MapJointsToColorSpace(IBody body, ICoordinateMapper mapper)
+        {
+            var ret = new Dictionary<JointType, Point2F>();
+
+            foreach (var joint in body.Joints)
+            {
+                Point3F cameraPoint = joint.Value.Position;
+                if (cameraPoint.Z < 0)
+                {
+                    cameraPoint.Z = 0.1f;
+                }
+
+                Point2F colorPoint =
+                       mapper.MapCameraPointToColorSpace(joint.Value.Position);
+
+                ret.Add(joint.Key, colorPoint);
+            }
+
+            return ret;
+        }
+
+        static Rectangle? TryGetHeadRectangleInColorSpace(IBody body, ICoordinateMapper mapper)
+        {
+            IJoint headJoint;
+            IJoint neckJoint;
+            if (!body.Joints.TryGetValue(JointType.Head, out headJoint) || !body.Joints.TryGetValue(JointType.Neck, out neckJoint)) return null;
+
+            var headJointCameraPos = mapper.MapCameraPointToColorSpace(headJoint.Position);
+            var neckJointCameraPos = mapper.MapCameraPointToColorSpace(headJoint.Position);
+            float headNeckDistance = headJointCameraPos.DistanceTo(neckJointCameraPos);
+            bool isFaceVertical = Math.Abs(headJointCameraPos.Y - neckJointCameraPos.Y) >
+                                  Math.Abs(headJointCameraPos.X - neckJointCameraPos.X);
+            var width = isFaceVertical ? headNeckDistance / 1.2f : headNeckDistance * 1.2f;
+            var height = isFaceVertical ? headNeckDistance * 1.2f : headNeckDistance / 1.2f;
+            return new Rectangle(
+                (int)(headJointCameraPos.X - headNeckDistance), 
+                (int)(headJointCameraPos.Y - headNeckDistance), 
+                (int)width, 
+                (int)height);
+
+        }
+    }
+
+
     public struct Point4F
     {
         public float X;
@@ -48,6 +90,11 @@ namespace KinectUnifier
         {
             X = x;
             Y = y;
+        }
+
+        public float DistanceTo(Point2F other)
+        {
+            return (float) Math.Sqrt((X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y));
         }
     }
 }
