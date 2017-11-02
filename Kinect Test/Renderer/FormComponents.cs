48117206fcb0ec4ec831c1aec325311394857379
brocny <miso.belak@gmail.com>
2017-11-02 13:28:41 +0100

    Implemented Renderer class

---
+++ Kinect Test/Renderer/FormComponents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics
+{
+    public class FormComponents
+    {
+        public FormComponents(Label label, PictureBox pictureBox)
+        {
+            PictureBox = pictureBox;
+            Label = label;
+        }
+
+        public PictureBox PictureBox { get; }
+        public Label Label { get; }
+
+    }
+}
