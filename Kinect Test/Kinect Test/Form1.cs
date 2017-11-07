7661a91826586e2b98481ddff327f7686026d8a3
brocny <miso.belak@gmail.com>
2017-11-07 23:39:07 +0100

    Implemented against LuxandFace2

--- Kinect Test/Kinect Test/Form1.cs
+++ Kinect Test/Kinect Test/Form1.cs
@@ -6,11 +6,15 @@ using System.Linq;
 using System.Windows.Forms;
 using KinectUnifier;
 using LuxandFaceLib;
+using System.Threading.Tasks;
 
 namespace Kinect_Test
 {
     public partial class Form1 : Form
     {
+        // Confidence of face match has to be above this value do display a name
+        private const float FaceMatchConfidenceThreshold = 0.75f;
+
         private Renderer _renderer;
 
         private IKinect _kinect;
@@ -28,8 +32,11 @@ namespace Kinect_Test
 
         private List<Rectangle> _facePositions;
         private List<double> _faceRotations;
-        private LuxandFace2 _face;
+        private LuxandFace2 _face1;
+        private LuxandFace2 _face2;
         private LuxandFaceDatabase _faceDatabase = new LuxandFaceDatabase();
+
+        private Task _lastTask;
 
         private readonly Brush[] _bodyBrushes =
         {
@@ -38,9 +45,7 @@ namespace Kinect_Test
         };
 
         private Pen[] _bodyPens;
-        
-        
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -48,8 +53,12 @@ namespace Kinect_Test
             _kinect = KinectFactory.KinectFactory.GetKinect();
             InitializeColorComponents();
 
-            _face = new LuxandFace2(_colorWidth, _colorHeight, _colorBytesPerPixel);
-            _face.InitializeLibrary();
+            _face1 = new LuxandFace2(_colorWidth, _colorHeight, _colorBytesPerPixel);
+            _face1.InitializeLibrary();
+
+            _face2 = new LuxandFace2(_colorWidth, _colorHeight, _colorBytesPerPixel);
+            _face2.InitializeLibrary();
+
             _renderer = new Renderer(new FormComponents(statusLabel, pictureBox1), _colorWidth, _colorHeight);
 
             InitializeBodyComponents();
@@ -84,8 +93,6 @@ namespace Kinect_Test
                 }
                 frame.CopyFramePixelDataToArray(_colorFrameBuffer);
             }
-            
-            _face.FeedFrame(_colorFrameBuffer);
         }
 
         void InitializeBodyComponents()
@@ -99,6 +106,13 @@ namespace Kinect_Test
             _bodyManager.BodyFrameReady += _bodyManager_BodyFrameReady;
         }
 
+        private void SwapFaceLibs()
+        {
+            var temp = _face1;
+            _face1 = _face2;
+            _face2 = temp;
+        }
+
         private void _bodyManager_BodyFrameReady(object sender, BodyFrameReadyEventArgs e)
         {
             using (var frame = e.BodyFrame)
@@ -110,31 +124,71 @@ namespace Kinect_Test
                 }
                 frame.CopyBodiesTo(_bodies);
             }
-            
+
             _renderer.ClearScreen();
-            _renderer.DrawBodiesWithFaceBoxes(_bodies, _colorFrameBuffer, _colorBytesPerPixel, _bodyBrushes, _bodyPens, _coordinateMapper);
+            _renderer.DrawBodiesWithFaceBoxes(_bodies, _colorFrameBuffer, _colorBytesPerPixel, _bodyBrushes,
+                _bodyPens, _coordinateMapper);
+            
+            _lastTask.Wait();
+            
             _facePositions = new List<Rectangle>();
-            _faceRotations = new List<double>();
 
             foreach (var body in _bodies)
             {
-                if (Util.TryGetHeadRectangleInColorSpace(body, _coordinateMapper, out var faceRect, out double rotAngle))
+                if (Util.TryGetHeadRectangle(body, _coordinateMapper, out var faceRect))
                 {
                     _facePositions.Add(faceRect);
-                    _faceRotations.Add(rotAngle);
                 }
             }
-            _face.FeedFacePositions(_facePositions.ToArray());
 
+            SwapFaceLibs();
 
-            for (int i = 0; i < _facePositions.Count; i++)
+            var feedTask = Task.Run(() =>
             {
-                _renderer.DrawFacialFeatures(_face.GetFacialFeatures(i), Brushes.Aqua, 1.5f);
-                var matchedFace = _faceDatabase.GetBestMatch(_face.GetFaceTemplate(i));
-                if(matchedFace.Item2 > 0.6f) { _renderer.DrawName($"{matchedFace.Item1} ({matchedFace.Item2:P})", _facePositions[i].Left, _facePositions[i].Bottom, _bodyBrushes[i]);}
-            }
+                _face1.FeedFaces(_colorFrameBuffer, _facePositions.ToArray(), _colorBytesPerPixel);
+            });
+
+            var featuresTask = Task.Run(() =>
+            {
+                var result = new Point[_face2.FaceCount][];
+                for (int i = 0; i < _face2.FaceCount; i++)
+                {
+                    result[i] = _face2.GetFacialFeatures(i);
+                }
+                return result;
+            });
+
+            var drawFeaturesTask = featuresTask.ContinueWith(t =>
+            {
+                var features = t.Result;
+                for (int i = 0; i < features.Length; i++)
+                {
+                    _renderer.DrawFacialFeatures(features[i], _bodyBrushes[i], 1.5f);
+                }
+            }, TaskContinuationOptions.HideScheduler);
+
+            var matchingTask = featuresTask.ContinueWith(t =>
+            {
+                for (int i = 0; i < _face2.FaceCount; i++)
+                {
+                    var matchedFace = _faceDatabase.GetBestMatch(_face1.GetFaceTemplate(i));
+                    if (matchedFace.Item2 > FaceMatchConfidenceThreshold)
+                    {
+                        _renderer.DrawName(
+                        $"{matchedFace.name} ({matchedFace.confidence:P})",
+                        _facePositions[i].Left,
+                        _facePositions[i].Bottom,
+                        _bodyBrushes[i]);
+                    }
+                }
+            }, TaskContinuationOptions.HideScheduler);
+
+            _lastTask = Task.WhenAll(matchingTask, drawFeaturesTask);
+
             pictureBox1.Invalidate();
             Application.DoEvents();
+
+            feedTask.Wait();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -158,7 +212,7 @@ namespace Kinect_Test
             {
                 if (_facePositions[i].Contains(pointInColorCoordinates))
                 {
-                    var faceInfo = _face.GetFaceTemplate(i);
+                    var faceInfo = _face1.GetFaceTemplate(i);
                     if (faceInfo == null) return;
                     if (!_faceDatabase.TryAddNewFace("Michal", faceInfo))
                     {
