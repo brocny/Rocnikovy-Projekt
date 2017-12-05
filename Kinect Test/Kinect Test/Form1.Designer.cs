fc60447730803c041f7d564658660354a469db5e
brocny <miso.belak@gmail.com>
2017-12-05 20:38:55 +0100

    Changed pictureBox1.SizeMode to accomodate different Image dimensions

--- Kinect Test/Kinect Test/Form1.Designer.cs
+++ Kinect Test/Kinect Test/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
