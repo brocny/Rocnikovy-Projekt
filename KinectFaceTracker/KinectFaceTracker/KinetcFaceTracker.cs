721f81866199d902ad55c385a4cb13652aab5fe6
brocny <21953560+brocny@users.noreply.github.com>
2018-04-07 11:17:14 +0200

    Fixed KinectFaceTracker project

--- KinectFaceTracker/KinectFaceTracker/KinetcFaceTracker.cs
+++ KinectFaceTracker/KinectFaceTracker/KinetcFaceTracker.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Face;
 using KinectUnifier;
+using LuxandFace;
 using LuxandFaceLib;
 
 namespace KinectFaceTracker
