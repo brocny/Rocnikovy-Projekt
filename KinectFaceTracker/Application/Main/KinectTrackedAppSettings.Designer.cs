667c79baff1b9bd2d8cfa1e273f95ea08970332d
brocny <21953560+brocny@users.noreply.github.com>
2018-12-02 21:28:23 +0100

    Finished a long time ago, w/e

--- KinectFaceTracker/Application/Main/KinectTrackedAppSettings.Designer.cs
+++ KinectFaceTracker/Application/Main/KinectTrackedAppSettings.Designer.cs
@@ -25,7 +25,7 @@ namespace App.Main {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool RenderFace {
             get {
                 return ((bool)(this["RenderFace"]));
@@ -37,7 +37,7 @@ namespace App.Main {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool RenderMatch {
             get {
                 return ((bool)(this["RenderMatch"]));
